09ec91e4120fcfedefa90d43136f4ce87c9508a4
Ajay <2021rathodjay2021@gmail.com>
2021-06-20 15:39:00 +0530

    solve the static file error

--- test_quiz/settings.py
+++ test_quiz/settings.py
@@ -125,6 +125,7 @@ STATICFILES_DIRS = [
     # '/var/www/static/',
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
